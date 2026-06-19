5df64ecd01a5f1fee6f27966b83438233f15c75a
Mahidi-Hasan-Supon <mahidihasansupon@gmail.com>
2026-06-19 11:37:43 +0600

    added

--- query.sql
+++ query.sql
@@ -64,3 +64,11 @@ select booking_id,full_name,fixture,total_cost from users inner join bookings on
 
 -- Query 5: Display a comprehensive list of all users and their booking IDs, ensuring that fans who have never bought a ticket are still listed.
 select users.user_id,full_name,booking_id from users left join bookings on bookings.user_id = users.user_id 
+
+
+-- Query 6: Find all ticket bookings where the total cost is strictly higher than the average cost of all ticket bookings.
+select booking_id,match_id,total_cost from bookings where total_cost > (
+  select avg(total_cost) from bookings
+)
+
+
